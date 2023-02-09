abe051e69d4899a8526e737986bba553d6136102
Eric Kinder <ekind@Erics-MacBook-Air.local>
2023-02-09 00:04:01 -0500

    added signup endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,19 @@
 const express = require('express')
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
 const router = express.Router();
+
+router.post(
+    '/',
+    async (req, res) => {
+      const { firstName, lastName, email, password, username } = req.body;
+      const user = await User.signup({ email, username, password, firstName, lastName });
+      await setTokenCookie(res, user);
+      
+      return res.json({
+        user: user
+      });
+    }
+  );
 
 module.exports = router;
