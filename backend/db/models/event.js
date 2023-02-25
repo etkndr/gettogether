d8a2d26245a126ae9a2ab12193a659e850977983
Eric Kinder <ekind@Erics-MacBook-Air.local>
2023-02-25 14:27:41 -0500

    changed associated event image name

--- backend/db/models/event.js
+++ backend/db/models/event.js
@@ -15,7 +15,7 @@ module.exports = (sequelize, DataTypes) => {
       Event.belongsTo(models.Group, {foreignKey: "groupId"})
       Event.belongsTo(models.Venue, {foreignKey: "venueId"})
       Event.hasMany(models.Attendance, {foreignKey: "eventId"})
-      Event.hasMany(models.EventImage, {foreignKey: "eventId"})
+      Event.hasMany(models.EventImage, {foreignKey: "eventId", as: "previewImage"})
     }
   }
   Event.init({
