cc8acc4a228bcb7b99f560ca4d4c1b7d0ee30f9c
Eric Kinder <ekind@Erics-MacBook-Air.local>
2023-03-23 00:48:01 -0400

    added fetch for current user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+import * as sessionActions from "./store/session";
+
 function App() {
-  return (
-    <h1>Hello from App</h1>
+  const dispatch = useDispatch()
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    dispatch(sessionActions.getSession()).then(() => setIsLoaded(true))
+  }, [dispatch])
+
+  return isLoaded && (
+    <Switch>
+      <Route exact path="/">
+        <h1>Hello from app!</h1>
+      </Route>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+    </Switch>
   );
 }
 
