678ca867a78d177a24426be6c1da4df4e6623156
Eric Kinder <ekind@Erics-MacBook-Air.local>
2023-04-06 00:45:44 -0400

    completed landing page minus join button styling

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./LandingPage.css"
 import introImg from "../../assets/online_events.svg"
+import joinImg from "../../assets/handsUp.svg"
+import findImg from "../../assets/ticket.svg"
+import startImg from "../../assets/joinGroup.svg"
 
 export default function LandingPage() {
     return (
         <div className="main">
-            <div className="sec-1">
+
+            <div className="intro">
                 <div className="intro-txt">
                     <h1>
                         The people platform—Where interests become friendships
@@ -13,7 +17,7 @@ export default function LandingPage() {
                     <p>
                         Whatever your interest, from hiking and reading 
                         to networking and skill sharing, 
-                        there are thousands of people who share it on Meetup. 
+                        there are thousands of people who share it on getTogether. 
                         Events are happening every day—log in to join the fun.
                     </p>
                 </div>
@@ -21,20 +25,59 @@ export default function LandingPage() {
                     <img src={introImg}/>
                 </div>
             </div>
-            <div className="sec-2">
-                <h1>
 
-                </h1>
+            <div className="how-it-works">
+                <h2>
+                    How getTogether works
+                </h2>
+                <p>
+                    Meet new people who share your interests 
+                    through online and in-person events. 
+                    It's free to create an account.
+                </p>
             </div>
-            <div className="sec-3">
-                <h1>
 
-                </h1>
+            <div className="join-group">
+                <div className="join-img">
+                    <img src={joinImg}/>
+                </div>
+                <h3>
+                    See all groups
+                </h3>
+                <p>
+                    Do what you love, meet others who love it, find your community. The rest is history!
+                </p>
             </div>
-            <div className="sec-4">
-                <h1>
+            <div className="find-event">
+                <div className="find-img">
+                    <img src={findImg}/>
+                </div>
+                <h3>
+                    Find an event
+                </h3>
+                <p>
+                    Events are happening on just about any topic you can think of, 
+                    from online gaming and photography to yoga and hiking.
+                </p>
+            </div>
+            <div className="start-group">
+                <div className="start-img">
+                    <img src={startImg}/>
+                </div>
+                <h3>
+                    Start a group
+                </h3>
+                <p>
+                    You don't have to be an expert to gather people together and explore shared interests.
+                </p>
+            </div>
 
-                </h1>
+            <div className="join">
+                <a href="/signup">
+                <button>
+                    Join getTogether
+                </button>
+                </a>
             </div>
         </div>
     )
