b7683e828cae6338751aaff8a167cfb8dea426bf
Eric Kinder <ekind@Erics-MacBook-Air.local>
2023-06-10 14:57:08 -0400

    added errors for update group form

--- frontend/src/components/EditGroup/index.js
+++ frontend/src/components/EditGroup/index.js
@@ -42,20 +42,65 @@ export default function EditGroup() {
         }
     }, [loaded, id])
 
-    
     useEffect(() => {
-        if (
-            name?.length &&
-            about?.length &&
-            city?.length &&
-            state?.length &&
-            !errors?.length
-            ) {
-                setDisabled(false)
-            } else {
-                setDisabled(true)
-            }
-        }, [name, about, city, state, image, errors])
+        
+        if (about?.length && about?.length < 30) {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                about: `${30 - about.length} more characters required`
+            }))
+        } else {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                about: ""
+            }))
+        }
+
+        if (location?.length && state?.length !== 2) {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                location: `Please enter a city and state (two characters) separated by comma`
+            }))
+        } else {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                location: ""
+            }))
+        }
+
+        if (image?.length && ["png", "jpg", "jpeg"].indexOf(image?.split(".")[image?.split(".").length - 1]) < 0) {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                image: "Image URL must end in '.png', '.jpg', or '.jpeg'"
+            }))
+        } else {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                image: ""
+            }))
+        }
+
+        if (name?.length) {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                name: ""
+            }))
+        }
+
+        if (type?.length) {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                type: ""
+            }))
+        }
+
+        if (privacy?.length) {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                privacy: ""
+            }))
+        }
+    }, [name, location, city, about, state, image, privacy, type])
         
     const updateGroup = (group) => {
         dispatch(groupActions.editGroup(group, id))
@@ -63,27 +108,73 @@ export default function EditGroup() {
             dispatch(groupActions.addImg(id, image))
             history.push(`/group/${res.id}`)
         })
-        .catch(async (res) => {
-            const data = await res.json()
-            if (data && data.errors) setErrors(data.errors)
-        })
     }
     
     const onSubmit = async (e) => {
         e.preventDefault()
-        let err = []
-        
+
         const group = {
             name,
             about,
             type,
             privacy: !!privacy,
             city,
-            state: state.toUpperCase(),
+            state: state?.toUpperCase(),
             previewImage: image
         }
-        
+
+        if (!location.length) {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                location: "City and state required"
+            }))
+        }
+
+        if (!name.length) {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                name: "Group name required"
+            }))
+        }
+
+        if (!type) {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                type: "Select group type"
+            }))
+        }
+
+        if (!privacy) {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                privacy: "Select public or private"
+            }))
+        }
+
+        if (!about.length) {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                about: "Group description required"
+            }))
+        }
+
+        if (!image.length) {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                image: "Group image required"
+            }))
+        }
+
+        if (
+            !errors.name?.length, 
+            !errors.location?.length, 
+            !errors.about?.length, 
+            !errors.image?.length, 
+            !errors.privacy?.length,
+            !errors.type?.length
+        ) {
         return updateGroup(group)
+        }
     }
     
     if (!sessionUser) {
@@ -96,9 +187,6 @@ export default function EditGroup() {
         <div className="content">
             <h2>Update your group</h2>
             <form onSubmit={onSubmit} className="start-form">
-            <ul>
-                        {errors?.map((error, idx) => <li className='errors' key={idx}>{error}</li>)}
-                    </ul>
             <div className="form-section">
                 <h3 className="form-heading">
                     Set your group's location
@@ -116,6 +204,7 @@ export default function EditGroup() {
                         }
                     }}
                     placeholder="City, STATE"></input>
+                    <p className="errors">{errors?.location}</p>
             </div>
 
             <div className="form-section">
@@ -129,6 +218,7 @@ export default function EditGroup() {
                     onChange={(e) => setName(e.target.value)}
                     placeholder="What is your group name?"
                     name="name"></input>
+                    <p className="errors">{errors?.name}</p>
             </div>
 
             <div className="form-section">
@@ -136,12 +226,12 @@ export default function EditGroup() {
                     Describe the purpose of your group
                 </h3>
                 <p className="form-caption">People will see this when we promote your group, but you'll be able to add to it later, too. 1. What's the purpose of the group? 2. Who should join? 3. What will you do at your events?</p>
-                <input 
-                    type="text"
+                <textarea
                     value={about} 
                     onChange={(e) => setAbout(e.target.value)}
                     placeholder="Please write at least 50 characters">
-                </input>
+                </textarea>
+                <p className="errors">{errors?.about}</p>
             </div>
 
             <div className="form-section">
@@ -152,6 +242,7 @@ export default function EditGroup() {
                     <option value="In person">In-person</option>
                     <option value="Online">Online</option>
                 </select>
+                <p className="errors">{errors?.type}</p>
 
                 <h3 className="form-heading">
                     Is this group private or public?
@@ -160,6 +251,7 @@ export default function EditGroup() {
                     <option value={true}>Private</option>
                     <option value={false}>Public</option>
                 </select>
+                <p className="errors">{errors?.privacy}</p>
 
                 <h3 className="form-heading">
                     Please add an image URL for your group below:
@@ -170,6 +262,7 @@ export default function EditGroup() {
                     onChange={(e) => setImage(e.target.value)}
                     placeholder="Image URL">
                 </input>
+                <p className="errors">{errors?.image}</p>
             </div>
 
             <button type="submit" disabled={disabled} className="dtl-btn">Update group</button>
