83e53066b2d9a49cc5dd811a8f90f56ef85bebb2
Eric Kinder <etkndr@gmail.com>
2023-12-27 01:33:39 -0500

    event-img

--- backend/db/seeders/20230211230624-event-img.js
+++ backend/db/seeders/20230211230624-event-img.js
@@ -1,41 +1,52 @@
-'use strict';
+"use strict";
 const bcrypt = require("bcryptjs");
-const {Event} = require("../models")
+const { Event } = require("../models");
 
 let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
 }
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    options.tableName = 'EventImages';
+  async up(queryInterface, Sequelize) {
+    options.tableName = "EventImages";
     return queryInterface.bulkInsert(options, [
       {
         eventId: 1,
         url: "https://i.imgur.com/grfMQkx.jpg",
-        preview: true
+        preview: true,
       },
       {
         eventId: 2,
         url: "https://i.imgur.com/2xRDfHF.jpg",
-        preview: true
+        preview: true,
       },
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-    ])},
+      {
+        eventId: 3,
+        url: "https://i.imgur.com/iNn5K12.jpg",
+        preview: true,
+      },
+      {
+        eventId: 4,
+        url: "https://i.imgur.com/as50YPT.jpg",
+        preview: true,
+      },
+      /**
+       * Add seed commands here.
+       *
+       * Example:
+       * await queryInterface.bulkInsert('People', [{
+       *   name: 'John Doe',
+       *   isBetaMember: false
+       * }], {});
+       */
+    ]);
+  },
 
-    async down (queryInterface, Sequelize) {
-      options.tableName = 'EventImages';
-      const Op = Sequelize.Op;
-      return queryInterface.bulkDelete(options, {});
-    }
+  async down(queryInterface, Sequelize) {
+    options.tableName = "EventImages";
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {});
+  },
 };
